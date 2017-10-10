f8a53efa8aa9af7fea8c9459e44c2b28d7a09110
Farhan5381 <farhanmahmood433@gmail.com>
2017-10-10 14:28:25 +0500

    -Changed how the ball rebounds of the brick

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -14,21 +14,49 @@ bool Brick::ColidedWithBall( Ball & ball )
 	{
 		destroyed = true;
 
-		if( rect.top > ball.GetRect().bottom )
+		if( ball.GetVelocity().x > 0.0f &&  ball.GetVelocity().y > 0.0f )
 		{
-			ball.ReboundY();
+			if( rect.top > ball.GetRect().bottom )
+			{
+				ball.ReboundY();
+			}
+			else if( rect.left > ball.GetRect().right )
+			{
+				ball.ReboundX();
+			}
 		}
-		else if( rect.bottom > ball.GetRect().top )
+		else if( ball.GetVelocity().x < 0.0f &&  ball.GetVelocity().y > 0.0f )
 		{
-			ball.ReboundY();
+			if( rect.top > ball.GetRect().bottom )
+			{
+				ball.ReboundY();
+			}
+			else if( rect.right > ball.GetRect().left )
+			{
+				ball.ReboundX();
+			}
 		}
-		else if( rect.left > ball.GetRect().right )
+		else if( ball.GetVelocity().x < 0.0f &&  ball.GetVelocity().y < 0.0f )
 		{
-			ball.ReboundX();
+			if( rect.bottom > ball.GetRect().top )
+			{
+				ball.ReboundY();
+			}
+			else if( rect.right > ball.GetRect().left )
+			{
+				ball.ReboundX();
+			}
 		}
-		else if( rect.right > ball.GetRect().left )
+		else if( ball.GetVelocity().x > 0.0f &&  ball.GetVelocity().y < 0.0f )
 		{
-			ball.ReboundX();
+			if( rect.bottom > ball.GetRect().top )
+			{
+				ball.ReboundY();
+			}
+			else if( rect.left > ball.GetRect().right )
+			{
+				ball.ReboundX();
+			}
 		}
 
 		coliding = true;
