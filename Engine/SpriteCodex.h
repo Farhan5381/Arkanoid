93eca45e3f5a76aae3ed11ecf70f428a04a02080
Farhan5381 <farhanmahmood433@gmail.com>
2017-10-11 18:05:44 +0500

    -Added putpixel functions for all objects sprites images

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -7,5 +7,18 @@ class SpriteCodex
 {
 public:
 	// centered drawing of 14x14 sprite
-	static void DrawBall( const Vec2& center,Graphics& gfx );
+	static void DrawBallNormal( const Vec2& top_left, Graphics& gfx );
+	static void DrawBallFlame( const Vec2& top_left, Graphics& gfx );
+	static void DrawpadNormal( const Vec2& top_left, Graphics& gfx );
+	static void DrawpadSmall( const Vec2& top_left, Graphics& gfx );
+	static void DrawpadBig( const Vec2& top_left, Graphics& gfx );
+	static void DrawRedBrick( const Vec2& top_left, Graphics& gfx );
+	static void DrawBlueBrick( const Vec2& top_left, Graphics& gfx );
+	static void DrawYellowBrick( const Vec2& top_left, Graphics& gfx );
+	static void DrawGreenBrick( const Vec2& top_left, Graphics& gfx );
+	static void DrawOrangeBrick( const Vec2 & top_left, Graphics & gfx );
+	static void DrawExpandPowerup( const Vec2& top_left, Graphics& gfx );
+	static void DrawContractPowerup( const Vec2& top_left, Graphics& gfx );
+	static void DrawMultiplyBallPowerup( const Vec2& top_left, Graphics& gfx );
+	static void DrawFlamingBallPowerup( const Vec2& top_left, Graphics& gfx );
 };
